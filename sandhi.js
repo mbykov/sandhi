6a657a27b33293158f28f78b5485587d42835efe
michael <m.bykov@gmail.com>
2014-10-23 14:09:44 +0400

    i

--- sandhi.js
+++ sandhi.js
@@ -38,66 +38,58 @@ var t_th_dh = ['त', 'थ', 'ध'];
 // cflex: -ti, flex: -dhi, etc, i.e. variant
 function removeSuffix(form, flex, cflex) {
     var stems = [];
+
+    // условие - наружу
     var re = new RegExp(flex + '$');
     var stem = form.replace(re, '');
     //log('---------', form, flex, cflex, stem)
     if (stem == form) return [];
-    /* здесь тоже - не просто вызов набора функций _t_th_etc_, а вызов в цикле
+
+
+    /* здесь тоже - не просто вызов набора функций _t_th_etc_, а вызов в цикле ?
        постоянные - hash.prima, hash.second,
        вычисляются - hash.ultima == hash.first
      */
 
     stems.push(stem); // default stem
     var first = cflex[0];
-    //var start = stem[0];
     var clean = stem.replace(/्$/, '');
     var last = clean.slice(-1);
 
     var hash = {};
     hash.stems = [stem];
-    //hash.stem = stem;
-    hash.prima = stem[0];
-    hash.ultima = u.ultima(stem);
+    hash.first = u.ultima(stem);
+    hash.virama = u.virama(stem);
     hash.second = cflex[0];
 
     // Aspirated Letters:
-    if (isIN(t_th, first)) stems = aspirated_t_th(stems);
-    if (isIN(t_th_dh, first) && true)  h_2_t_th_dh(hash);
+    if (isIN(t_th, first)) aspirated_t_th(stems, hash);
+    if (isIN(t_th_dh, first) && stem[0] == 'द')  h_2_t_th_dh(hash);
 
-    log('STEMS ASP========', stems);
-    return stems;
+    log('HASH STEMS', hash.stems);
+    return hash.stems;
 }
 
-// The h both ends a root that starts with d and is in front of t, th, or dh
+// h is treated like gh: The h both ends a root that starts with d and is in front of t, th, or dh;
 function h_2_t_th_dh(hash) {
-    var res = [];
-    log('==== HHHH', hash.first)
-    /* NOW: ==================================================================
-       вместо многих переменных - один hash,
-       и главное - stems обрабатываются в цикле. И нужно каждый раз вычислять first-second для условий вызова обработчика
-     */
-    return res;
+    ulog(hash);
+
+    return;
 }
 
 // t- and th-, when they are the second letter, become dh-
-function aspirated_t_th(stems) {
-    var res = [];
-    var virama = true;
-    _.each(stems, function(stem) {
-        var clean = stem.replace(/्$/, '');
-        if (clean == stem) virama = false;
-        var last = clean.slice(-1);
-        var asp = u.unasp2asp(last);
-        var tail = (virama) ? [last, Const.virama].join('') : last;
-        if (virama) {
-            last = [last, Const.virama].join('');
+function aspirated_t_th(stems, hash) {
+    _.each(hash.stems, function(stem) {
+        var first = hash.first;
+        var asp = u.unasp2asp(first);
+        if (hash.virama) {
+            first = [first, Const.virama].join('');
             asp = [asp, Const.virama].join('');
         }
-        var newStem = stem.replace(reEnd(tail), asp);
-        res.push(stem);
-        res.push(newStem);
+        var newStem = stem.replace(reEnd(first), asp);
+        hash.stems.push(newStem);
     });
-    return res;
+    return;
 }
 
 
