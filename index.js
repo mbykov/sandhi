736d5583fa4514f8d5ac341a7ccfb9fa6846427d
Michael Bykov <m.bykov@gmail.com>
2015-05-15 16:34:24 +0300

    i

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 /*
-  node.js and component
+  npm module
 */
 
 var _ = require('underscore');
 var util = require('util');
-// var typeOf = require('typeof');
-// var shiva = require('mbykov/shiva-sutras');
-// var shiva = require('shiva-sutras');
 var Const = require('./lib/const');
 var u = require('./lib/utils');
-var vowRules = require('./lib/vow_sutras');
+var vowRules = require('./lib/vowel_sutras');
 var visRules = require('./lib/visarga_sutras');
 var consRules = require('./lib/cons_sutras');
-// var delConsRules = require('./lib/del_cons_rules');
 var sutras = require('./lib/cons_sutras');
 var log = u.log;
 
@@ -49,9 +45,7 @@ function makeMarkerList(samasa) {
                 pattern = [Const.virama, next1].join('');
             }
             mark = {type: 'yana', pattern: pattern, beg: next2, idx: idx, pos: i};
-            idx++;
             // log('M vow yaNa', i, 'mark', mark); // योगि + अङ्ग - योग्यङ्ग
-            marks.push(mark);
         } else if (sym == Const.A && u.c(Const.yava, next1)) { // diphthong-vridhi, beg: vow or cons-for-a - to only ya,va
             // TODO: засада - перебивает dirgha-type - देहाविष्ट - आविष्ट - TODO: убрать везде else?
             // 6.1.78 - ayadi - e,o+vow-a => ay,av+vow; E,O+vow => Ay,Av+vow
@@ -62,86 +56,78 @@ function makeMarkerList(samasa) {
             }
             mark = {type: 'ayadi', pattern: pattern, beg: next2, idx: idx, pos: i};
             // log('M vow ayadi-vriddhi', i, 'mark', mark);
-            marks.push(mark);
         } else if (u.c(Const.yava, sym) && u.c(Const.allligas, next1)) { // diphthong followed by any vowel (e,o vow-a), including itself, changes to its semi-vowel equivalent - external - optional
             // 6.1.78 - ayadi - e,o+vow-a => ay,av+vow; E,O+vow => Ay,Av+vow
             pattern = [sym, next1].join('');
             mark = {type: 'ayadi', pattern: pattern, idx: idx, pos: i};
             // log('M vow ayadi-guna-wo-a', i, 'mark', mark);
-            marks.push(mark);
         } else if (sym == Const.avagraha) {// "e" and "o" at the end of a word, when followed by "a" gives avagraha
             // 6.1.109 - ayadi - e,o+a => avagraha
             pattern = sym;
             mark = {type: 'ayadi', pattern: pattern, idx: idx, pos: i};
             // log('M vow ayadi-avagraha', i, 'mark:', mark);
-            marks.push(mark);
 
         } else if (next1 && next2 && u.c(Const.Jay, sym) && (next1 == Const.virama) && u.c(Const.hal, next2)) { // Jay = hard+soft
             if (sym == 'र') return; // FIXME: - д.б. список всех невозможных комбинаций, возможно, не здесь, а перед определителем маркера, вне if
             pattern = [sym, Const.virama, next2].join('');
             mark = {type: 'cons', pattern: pattern, fin: sym, beg: next2, idx: idx, pos: i};
-            idx++;
-            marks.push(mark);
             // log('M cons', i, sym, next1, next2);
             // } else if (u.c(Const.dirgha_ligas, sym) && sym != 'ॢ') {
-        } else if (u.c(Const.dirgha_ligas, sym)) { // FIXME: проверить !la - на la-liga нет теста
-            // 6.1.101
-            mark = {type: 'dirgha', pattern: sym, idx: idx, pos: i};
-            idx++;
-            // log('M vow dirgha', i, 'mark', mark, 'size', samasa.length); // योगि + ॠकार - योग्यॄकार
-            marks.push(mark);
         } else if (u.c(Const.gunas, u.vowel(sym))) {
             // 6.1.87
             mark = {type: 'guna', pattern: sym, idx: idx, pos: i, size: i+1};
-            idx++;
             // log('M vow guna', i, 'mark', mark);
-            marks.push(mark);
             // guna r,l ->
         } else if (u.c(Const.vriddhis, u.vowel(sym))) {
             // 6.1.88
             mark = {type: 'vriddhi', pattern: sym, idx: idx, pos: i};
-            idx++;
             // log('M vow guna', i, 'mark', mark);
-            marks.push(mark);
         } else if ((u.c(Const.hal, sym) || sym == Const.A) && (next1 == 'र' || next1 == 'ल') && next2 == Const.virama) {
             // 6.1.87 r,l
             pattern = [next1, Const.virama].join('');
             mark = {type: 'guna', pattern: pattern, idx: idx, pos: i+1};
-            idx++;
             // log('M vow guna special', i, 'mark', mark);
-            marks.push(mark);
-        } else {
-            // log('can not mark');
-            // return;
         }
+        // === VOWEL ===
 
+        // simple vowel, followed by a similar vowel => dirgha
+        if (u.c(Const.dirgha_ligas, sym)) { // FIXME: проверить !la - на la-liga нет теста
+            var mark = {type: 'vowel', num: '6.1.101', pattern: sym, idx: idx, pos: i};
+            marks.push(mark);
+            // log('M vow dirgha', i, 'mark', mark, 'size', samasa.length);
+        }
         // === VISARGA ===
 
         // अ & visarga changes to ओ+avagraha when followed by अ
         if (u.c(Const.hal, sym) && next1 == 'ो' && next2 == Const.avagraha) {
             pattern = [next1, Const.avagraha].join('');
-            mark = {type: 'visarga', num: '4.1.2', pattern: pattern, idx: idx, pos: i};
-            idx++;
-            // log('M visarga', i, 'mark', mark);
+            var mark = {type: 'visarga', num: '4.1.2', pattern: pattern, idx: idx, pos: i};
             marks.push(mark);
+            // log('M visarga', i, 'mark', mark);
         }
+        idx++;
         // log('SYM', i, sym, next1, next2);
     });
+    // log(marks)
     return marks;
 }
 
 function mark2sandhi(marks) {
+    var list = [];
     marks.forEach(function(mark) {
-        if (mark.type != 'visarga') return;
         var sutra = vowRules[mark.num] || consRules[mark.num] || visRules[mark.num];
-        // log('SUTRA', sutra, mark.num);
         if (!sutra) return; // FIXME: не должно быть
-        sutra.split(mark);
-        // log('MM', mark);
+        var sandhis = sutra.split(mark);
+        sandhis.forEach(function(sandhi) {
+            var m = JSON.parse(JSON.stringify(mark));
+            m.sandhi = sandhi;
+            list.push(m);
+        });
     });
+    return list;
 }
-/*
-  FIXME: здесь нужно выделить слова, т.е. пробелы и спец. символы - конец строки
+
+/*  FIXME: здесь нужно выделить слова, т.е. пробелы и спец. символы - конец строки
   добавить outer после split или до? Имеет ли это значение?
 */
 sandhi.prototype.split = function(str) {
@@ -165,18 +151,15 @@ sandhi.prototype.split = function(str) {
 function splitone(samasa) {
     var res = [];
     var marks = makeMarkerList(samasa);
-    if (marks.length == 0) return; // log('==no_markers!!!=='); // FIXME: этого не должно быть
+    if (marks.length == 0) return log('==no_markers!!!=='); // FIXME: этого не должно быть
     // log('==marks==', marks.map(function(m) { return JSON.stringify(m)}));
-    mark2sandhi(marks);
-    // log('==== marks:', marks);
+    var list = mark2sandhi(marks);
     // log('==list==', list.map(function(m) { return JSON.stringify(m)}));
-    // if (list.length == 0) return; // log('==no_markers!!!=='); // FIXME: этого не должно быть // 6.1.78.+_12_
-    var cleans = u.combinator(marks);
-    if (cleans.length > 15) log('==cleans.size== marks:', marks.length, 'cleans:', cleans.length)
+    var cleans = u.combinator(list);
+    if (cleans.length > 15) log('==cleans.size== list:', list.length, 'cleans:', cleans.length)
     cleans.forEach(function(comb) {
         var result = samasa;
         comb.forEach(function(mark) {
-            if (mark.type != 'visarga') return;
             // log('=>M', mark);
             result = u.replaceByPos(result, mark.pattern, mark.sandhi, mark.pos);
         });
@@ -194,29 +177,11 @@ function splitone(samasa) {
 // шмитовский проезд - кони - б.белое здание, по ул. 5=года - управа, мюллер, 916-917-42-22
 
 sandhi.prototype.add = function(first, second) {
-    var res = [];
     var mark = makeMarker(first, second);
-    var ftype;
-    if (u.c(['dirgha', 'guna', 'vriddhi', 'yana', 'ayadi', 'vow'], mark.type)) ftype = 'vow';
-    else if (u.c(['visarga-o', 'visarga', 'visarga', 'visarga', 'visarga', 'visarga'], mark.type)) ftype = 'visarga';
-    else return;
-    var fn = ['./lib/', ftype, '_sutras'].join('');
-    var sutras = require(fn);
-    sutras.forEach(function(sutra) {
-        if (sutra.num == '') return;
-        // if (sutra.type != mark.type) return; // убрал, чтобы не прописывать подтип - диргха, etc, как в сплите
-        var test = JSON.parse(JSON.stringify(mark));
-        // log('====> E', sutra.num, test);
-        var tmps = sutra.add(test);
-        if (!tmps) return;
-        // log('MARK', sutra.num, mark)
-        tmps.forEach(function(mark) {
-            var samasa = makeAddResult(mark);
-            res.push(samasa);
-        });
-    });
-    // log('ADD RES', res);
-    return res;
+    var sutra = vowRules[mark.num] || consRules[mark.num] || visRules[mark.num];
+    if (!sutra) return; // FIXME: не должно быть
+    var marks = sutra.add(mark);
+    return marks.map(function(m) { return makeAddResult(m)});
 }
 
 
@@ -244,29 +209,36 @@ function makeMarker(f, s) {
         marker = {type: 'cons', first: first, fin: fin, vir: vir, second: second, beg: beg};
         if (vir) marker.vir = true;
         if (candra) marker.candra = true;
+
+        // === VOWELS ===
     } else if ((u.c(Const.consonants, fin) || u.c(Const.allligas, fin)) && u.c(Const.allvowels, beg)) {
-        // здесь можно найти и указать не type=vow, а подтипы. Чтобы не гонять все сутры, но это одно и тоже, кажется. В сплите я указываю типы, потому что маркеров много
-        marker = {type: 'vow', first: first, fin: fin, second: second, beg: beg};
+        marker = {type: 'vowel', first: first, fin: fin, second: second, beg: beg};
         if (u.c(Const.consonants, fin)) marker.fin = '';
+        if (u.similar(fin, beg)) marker.num = '6.1.101';
         // log('VOW MARK', marker);
-    } else if ((first.length == 1) && u.c(Const.allvowels, fin) && u.c(Const.allvowels, beg)) {        // FIXME: случай first из одной гласной буквы.
-        fin = u.liga(fin);
-        marker = {type: 'vow', first: first, fin: fin, second: second, beg: beg};
-        // log('VOW MARK ONE', marker);
+
+    // } else if ((first.length == 1) && u.c(Const.allvowels, fin) && u.c(Const.allvowels, beg)) {        // FIXME: случай first из одной гласной буквы.
+    //     fin = u.liga(fin);
+    //     marker = {type: 'vow', first: first, fin: fin, second: second, beg: beg};
+    //     // log('VOW MARK ONE', marker);
+
+
+
     } else if (fin == Const.visarga) {
         marker = {type: 'visarga', first: first, fin: fin, second: second, beg: beg};
+        if (beg =='अ') marker.num = '4.1.2';
     }
     // log('MARKER', marker);
     return marker;
 }
 
 function makeAddResult(test) {
-    if (test.type == 'cons' && u.c(Const.allvowels, test.beg)) {
-        test.second.shift();
-        liga = Const.vow2liga[test.beg];
-        test.second.unshift(liga);
-        test.vir = false;
-    }
+    // if (test.type == 'cons' && u.c(Const.allvowels, test.beg)) {
+    //     test.second.shift();
+    //     liga = Const.vow2liga[test.beg];
+    //     test.second.unshift(liga);
+    //     test.vir = false;
+    // }
     var conc = (test.conc) ? ' ' : '';
     if (test.end) test.first.push(test.end);
     if (test.vir) test.first.push(Const.virama);
